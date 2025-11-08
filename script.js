2d70d1bf81e11a0f79450dda223231744ea3e256
minorlu <scherbodenis@gmail.com>
2025-11-08 16:52:24 +0300

    Add button to select the grid size

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const container = document.querySelector(".container");
+const r = document.querySelector(":root");
+const btn = document.querySelector("#set-size-btn");
+let styles = getComputedStyle(r);
+let gridRowSize = styles.getPropertyValue("--grid-row-size");
 
-for (let i = 0; i < 256; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid-square");
+function DrawGrid(size) {
+    while (container.firstChild)
+        container.removeChild(container.firstChild);
 
-    container.appendChild(gridSquare);
+    for (let i = 0; i < gridRowSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        for (let j = 0; j < gridRowSize; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            gridRow.appendChild(gridSquare);
+    }
+
+    container.appendChild(gridRow);
 }
+}
+
+DrawGrid(gridRowSize);
 
 container.addEventListener("mouseover", (e) => {
     e.target.classList.add("hovered-square");
+});
+
+btn.addEventListener("click", (e) => {
+    gridRowSize = prompt("Input grid size:", "16");
+    r.style.setProperty("--grid-row-size", gridRowSize);
+    DrawGrid(gridRowSize);
 });
